da8a85992870100b7e840707bcfaa7da54405492
riqbic <dukereduker@gmail.com>
2023-01-26 20:47:59 -0500

    1/26 sync up

--- commons-js-main.js
+++ commons-js-main.js
@@ -3,18 +3,19 @@ function aboutUsPop(){
     var features = document.getElementById("features");
     var blog = document.getElementById("blog");
     var get_involved = document.getElementById("get-involved");
-    
+
     if(element.classList.contains("about-us-full")){
         element.classList.remove("about-us-full");
+        setTimeout(function() {displayToggle("about-us", "block")}, 1500);
         opacityToggle("about-us","1");
-        setTimeout(displayToggle(),500,"about-us","block");
     }
-    
+
     else{
+        // element.style.width = element.offsetWidth;
         element.classList.add("about-us-full");
         opacityToggle("about-us","0");
-        setTimeout(displayToggle(),500,"about-us","none");
-    }
+        setTimeout(function() {displayToggle("about-us", "none")}, 500);
+    // }
 }
 
 function displayToggle(active_id,display_mode){
@@ -32,15 +33,16 @@ function displayToggle(active_id,display_mode){
 }
 
 function opacityToggle(active_id,opacity_level){
+    console.log(opacity_level);
     var element = document.getElementById("about-us");
     var features = document.getElementById("features");
     var blog = document.getElementById("blog");
     var get_involved = document.getElementById("get-involved");
 
     if(active_id==="about-us"){
-        console.log(opacity_level);
         features.style.opacity = opacity_level;
         blog.style.opacity = opacity_level; 
         get_involved.style.opacity = opacity_level;
     }
+}
 }
