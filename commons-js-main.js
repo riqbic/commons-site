072bf3c4c84698de7551718aa835908cae228e48
riqbic <dukereduker@gmail.com>
2023-01-30 16:54:56 -0500

    grid layout with working popout feature

--- commons-js-main.js
+++ commons-js-main.js
@@ -1,48 +1,46 @@
-function aboutUsPop(){
-    var element = document.getElementById("about-us");
-    var features = document.getElementById("features");
-    var blog = document.getElementById("blog");
-    var get_involved = document.getElementById("get-involved");
+function popOut(activeID){
+    var popout_container = document.getElementById("popout-container");
+    var grid_container = document.getElementById(activeID);
+    var active_element = document.getElementById(activeID+"-content");
 
-    if(element.classList.contains("about-us-full")){
-        element.classList.remove("about-us-full");
-        setTimeout(function() {displayToggle("about-us", "block")}, 1500);
-        opacityToggle("about-us","1");
+    if(popout_container.contains(active_element)){
+        popout_container.style.width = grid_container.offsetWidth+"px";
+        popout_container.style.height = grid_container.offsetHeight+"px";
+        popout_container.style.top = grid_container.offsetTop+"px";
+        popout_container.style.left = grid_container.offsetLeft+"px";
+        opacityToggle("1");
+        setTimeout(function(){
+            popout_container.style.display = "none";
+            grid_container.appendChild(active_element);
+            grid_container.style.visibility = "visible";
+        },1000);
     }
 
     else{
-        // element.style.width = element.offsetWidth;
-        element.classList.add("about-us-full");
-        opacityToggle("about-us","0");
-        setTimeout(function() {displayToggle("about-us", "none")}, 500);
-    // }
-}
-
-function displayToggle(active_id,display_mode){
-    var element = document.getElementById("about-us");
-    var features = document.getElementById("features");
-    var blog = document.getElementById("blog");
-    var get_involved = document.getElementById("get-involved");
-
-    if(active_id==="about-us"){
-        console.log(display_mode);
-        features.style.display = display_mode;
-        blog.style.display = display_mode; 
-        get_involved.style.display = display_mode;
+        if(popout_container.hasChildNodes()){
+            console.log("check");
+        }
+        else{
+            popout_container.style.width = grid_container.offsetWidth+"px";
+            popout_container.style.height = grid_container.offsetHeight+"px";
+            popout_container.style.top = grid_container.offsetTop+"px";
+            popout_container.style.left = grid_container.offsetLeft+"px";
+            popout_container.style.display = "block";
+            grid_container.style.visibility = "hidden";
+            opacityToggle("0");
+            popout_container.appendChild(active_element);
+            setTimeout(function(){
+                popout_container.style.width = "100%";
+                popout_container.style.height = document.getElementById("newsy-container").offsetHeight-20+"px";
+                popout_container.style.top = "15vh";
+                popout_container.style.left = "0px";
+            },50);
+            window.scrollTo(0, 0);
+        }
     }
 }
 
-function opacityToggle(active_id,opacity_level){
-    console.log(opacity_level);
-    var element = document.getElementById("about-us");
-    var features = document.getElementById("features");
-    var blog = document.getElementById("blog");
-    var get_involved = document.getElementById("get-involved");
-
-    if(active_id==="about-us"){
-        features.style.opacity = opacity_level;
-        blog.style.opacity = opacity_level; 
-        get_involved.style.opacity = opacity_level;
-    }
-}
+function opacityToggle(opacity_level){
+    var grid = document.getElementById("newsy-container");
+    grid.style.opacity = opacity_level;
 }
