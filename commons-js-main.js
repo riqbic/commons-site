f8d390aad36165678425a58f0ed77fa9582634b1
riqbic <dukereduker@gmail.com>
2023-02-15 23:37:58 -0500

    working history states, and dynamic content start

--- commons-js-main.js
+++ commons-js-main.js
@@ -1,72 +1,81 @@
 //global variables
 var popout_state = 0;
-var push_state = 0;
-
-// decide what state to push based on the same logic as popOut(), this exists so we can call popOut without also pushing a state
-function pushStateHandler(activeID){
-    var popout_container = document.getElementById("popout-container");
-    var active_element = document.getElementById(activeID+"-content");
-    if(popout_state===1){
-        console.log("push popout");
-        history.pushState(activeID,activeID,activeID);
-    }
-    else if(popout_state===0){
-        console.log("push reset");
-        history.pushState(activeID,"The Commons","http://127.0.0.1:5500/grid-test.html");
-    }
-}
+var push_state = 1;
 
 window.addEventListener('popstate', onPopState);
 function onPopState(ev) {
    activeID = ev.state;
    console.log('activeID:'+activeID);
    popOut(activeID,0);
+   console.log(push_state);
+   if(!push_state){
+    console.log("history forward");
+    //window.history.forward();
+   }
+}
+
+window.addEventListener('resize',resize);
+function resize(){
+    var about_us_flex_item = document.getElementsByClassName("about-us-flex-item");
+    var about_us_grid_item = document.getElementById("about-us");
+    var popout_container = document.getElementById("popout-container");
+    popout_container.style.width = document.getElementById("newsy-container").offsetWidth-20+"px";
+    for (var i = 0; i < about_us_flex_item.length; i ++) {
+        about_us_flex_item[i].style.width = about_us_grid_item.offsetWidth+"px";
+    }
 }
 
 function popOut(activeID,call_from_page){
     var popout_container = document.getElementById("popout-container");
     var grid_container = document.getElementById(activeID);
     var active_element = document.getElementById(activeID+"-content");
-
-    if(popout_container.contains(active_element)  && popout_state===1){
-        if(call_from_page){
+    var full_content = document.getElementsByClassName(activeID+"-content-full");
+    if(popout_container.hasChildNodes() && popout_state){
+        if(call_from_page && push_state){
             history.pushState(activeID,"The Commons","http://127.0.0.1:5500/grid-test.html");
-            console.log("push reset");
+            //console.log("push reset");
         }
-        popout_state = 0; b
+        popout_state = 0;
+        push_state = 0;
         var contentID = popout_container.firstChild.id;
         var activeID = contentID.split("-content").join("")
         var active_element = document.getElementById(contentID);
         var grid_container = document.getElementById(activeID);
-        resetPopoutSize(contentID.split("-content").join(""));
+        for (var i = 0; i < full_content.length; i ++) {
+            full_content[i].style.opacity = 0;
+        }
+        resetPopoutSize(activeID);
         opacityToggle("1");
         setTimeout(function(){
             popout_container.style.display = "none";
             grid_container.appendChild(active_element);
             grid_container.style.visibility = "visible";
-        },1000);
+            push_state = 1;
+        },10);
     }
-    else{
-        if(!popout_container.hasChildNodes() && popout_state===0){
-            if(call_from_page){
-                history.pushState(activeID,activeID,activeID);
-                console.log("push popout");
-            }
-            popout_state = 1;
-            resetPopoutSize(activeID);
-            popout_container.style.display = "block";
-            grid_container.style.visibility = "hidden";
-            opacityToggle("0");
-            popout_container.appendChild(active_element);
-            setTimeout(function(){
-                popout_container.style.width = document.getElementById("newsy-container").offsetWidth-20+"px";;
-                popout_container.style.height = document.getElementById("newsy-container").offsetHeight-20+"px";
-                popout_container.style.top = document.getElementById("newsy-container").offsetTop+10+"px";
-                popout_container.style.left = "10px";
-            },50);
-            window.scrollTo(0, 0);
+    else if(!popout_container.hasChildNodes() && !popout_state){
+        if(call_from_page){
+            history.pushState(activeID,activeID,activeID);
+            //console.log("push popout");
         }
+        popout_state = 1;
+        resetPopoutSize(activeID);
+        popout_container.style.display = "block";
+        grid_container.style.visibility = "hidden";
+        opacityToggle("0");
+        popout_container.appendChild(active_element);
+        setTimeout(function(){
+            popout_container.style.width = document.getElementById("newsy-container").offsetWidth-20+"px";
+            popout_container.style.height = document.getElementById("newsy-container").offsetHeight-20+"px";
+            popout_container.style.top = document.getElementById("newsy-container").offsetTop+10+"px";
+            popout_container.style.left = "10px";
+            for (var i = 0; i < full_content.length; i ++) {
+                full_content[i].style.opacity = 1;
+            }
+        },10);
+        window.scrollTo(0, 0);
     }
+    //console.log(history.state);
 }
 
 /*toggle the opacity of the grid*/
