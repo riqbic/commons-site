87d9f1a4650ee45229083b584c056bce9bf0ca2c
riqbic <dukereduker@gmail.com>
2023-02-23 23:13:22 -0500

    updating production

--- wp-content/themes/the-commons/front-page.php
+++ wp-content/themes/the-commons/front-page.php
@@ -2,15 +2,34 @@
 
 <div class="newsy-container grid-off-mobile" id="newsy-container">
     <div class="grid-item grid-item-1" id="about-us">
-        <div class="grid-content" id="about-us-content"  onclick="popOut('about-us')">
+        <div class="grid-content" id="about-us-content"  onclick="popOut('about-us',1,1)">
             <h2>About us</h2>
-            <p class="newsy">
-                Born in Boston, The Commons is a parkour org that invests in North American parkour culture. Through hosting events, uplifting local communities, and providing opportunities for athletes and artists, we are working to put American parkour on the map.
-            </p>
+            <div class="about-us-flexbox">
+                <div class="about-us-flex-item">
+                    <p class="newsy">
+                        Born in Boston, The Commons is a parkour org that invests in North American parkour culture. Through hosting events, uplifting local communities, and providing opportunities for athletes and artists, we are working to put American parkour on the map.
+                    </p>
+                </div>
+                <div class="about-us-content-full about-us-flex-item">
+                    <p class="newsy">
+                        Being a community project means that anybody and everybody can contribute to the cause, but here are some of the people that keep the wheels turning:
+                    </p>
+                </div>
+                <div class="about-us-content-full about-us-flex-item">
+                    <p class="newsy">
+                        Everybody loves David. David Ehrlich is the founder, heart, and soul of The Commons. He preaches radical inclusivity, and has a special gift for bringing people together. As our primary community and event coordinator, David’s job is to create community events, do outreach, and make sure that The Commons provides safe and fun spaces for all.
+                    </p>
+                </div>
+                <div class="about-us-content-full about-us-flex-item">
+                    <p class="newsy">
+                        Joining The Commons soon after it’s inception; Alec is the guy who makes things happen. He’s the creative director by day, web developer by night, park builder by the next day, and event coordinator after that… Alec seems to be everywhere all at once.
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
     <div class="grid-item grid-item-2" id="features">
-        <div class="grid-content" id="features-content"  onclick="popOut('features')">
+        <div class="grid-content" id="features-content"  onclick="popOut('features',1,1)">
             <h2>Features</h2>
             <img src="<?php echo get_template_directory_uri(); ?>/img/group_pallets_crop.png" alt="group pallets" id="group-pallets" width="300" height="200">
             <p class="newsy">
@@ -19,7 +38,7 @@
         </div>
     </div>
     <div class="grid-item grid-item-3" id="blog">
-        <div class="grid-content" id="blog-content"  onclick="popOut('blog')">
+        <div class="grid-content" id="blog-content"  onclick="popOut('blog',1,1)">
             <h2>Blog</h2>
             <div class="blog-flex-container">
                 <?php 
@@ -53,7 +72,7 @@
         </div>
     </div>
     <div class="grid-item grid-item-4" id="get-involved">
-        <div class="grid-content" id="get-involved-content"  onclick="popOut('get-involved')">
+        <div class="grid-content" id="get-involved-content"  onclick="popOut('get-involved',1,1)">
             <h2>Get Involved</h2>
             <p class="newsy">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus sapiente iusto dolore nihil. Iure ullam eaque temporibus ea expedita impedit atque, vitae velit voluptates tempore odio excepturi labore, modi reiciendis cumque id consectetur consequuntur sunt asperiores itaque. Sunt atque, a incidunt id neque non doloremque veritatis! Nam delectus dolorem pariatur.
@@ -61,7 +80,7 @@
         </div>
     </div>
     <div class="grid-item grid-item-5" id="market">
-        <div class="grid-content" id="market-content"  onclick="popOut('market')">
+        <div class="grid-content" id="market-content"  onclick="popOut('market',1,1)">
             <h2>Market</h2>
             <p class="newsy">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus sapiente iusto dolore nihil. Iure ullam eaque temporibus ea expedita impedit atque, vitae velit voluptates tempore odio excepturi labore, modi reiciendis cumque id consectetur consequuntur sunt asperiores itaque. Sunt atque, a incidunt id neque non doloremque veritatis! Nam delectus dolorem pariatur.
@@ -72,5 +91,7 @@
 
 <!-- the div below (popout-container) must be left COMPLETELY empty!!!-->
 <div class="popout-container" id="popout-container"></div>
+
+<script>resize();</script>
 
 <?php get_footer(); ?>
--- wp-content/themes/the-commons/js/commons-js-main.js
+++ wp-content/themes/the-commons/js/commons-js-main.js
@@ -1,36 +1,120 @@
-function popOut(activeID){
+//global variables
+var popout_state = 0;
+var push_state = 1;
+
+window.addEventListener('popstate', onPopState);
+function onPopState(ev) {
+   activeID = ev.state;
+   popOut(activeID,0,1);
+}
+
+window.addEventListener('load',onPageLoad);
+function onPageLoad() {
+    let url = window.location.search;
+    let params = new URLSearchParams(url);
+    var activeID = params.get("pop");
+
     var popout_container = document.getElementById("popout-container");
     var grid_container = document.getElementById(activeID);
     var active_element = document.getElementById(activeID+"-content");
+    var full_content = document.getElementsByClassName(activeID+"-content-full");
+    var newsy_container = document.getElementById("newsy-container");
 
-    if(popout_container.contains(active_element)){
-        resetPopout(activeID);
+    if (params.getAll("pop").length !== 0){
+        //popOut(params.get("pop"),0,0);
+        popout_state = 1;
+        popout_container.style.display = "block";
+        grid_container.style.visibility = "hidden";
+        popout_container.appendChild(active_element);
+        popout_container.style.width = newsy_container.offsetWidth-20+"px";
+        popout_container.style.height = newsy_container.offsetHeight-20+"px";
+        popout_container.style.top = newsy_container.offsetTop+10+"px";
+        popout_container.style.left = "10px";
+        for (var i = 0; i < full_content.length; i ++) {
+            full_content[i].style.opacity = 1;
+        }
+    }
+}
+
+window.addEventListener('transitionend',transitionToggle);
+function transitionToggle(){
+    var newsy_container = document.getElementById("newsy-container");
+    var popout_container = document.getElementById("popout-container");
+    popout_container.classList.remove("transitions");
+    newsy_container.classList.remove("transitions");
+}
+
+window.addEventListener('resize',resize);
+function resize(){
+    var about_us_flex_item = document.getElementsByClassName("about-us-flex-item");
+    var about_us_grid_item = document.getElementById("about-us");
+    var popout_container = document.getElementById("popout-container");
+    popout_container.style.width = document.getElementById("newsy-container").offsetWidth-20+"px";
+    for (var i = 0; i < about_us_flex_item.length; i ++) {
+        about_us_flex_item[i].style.width = about_us_grid_item.offsetWidth+"px";
+    }
+}
+
+
+//params (the ID of the grid item being popped out, did the function call come from the page? 0 or 1, should the transition play? 0 or 1)
+function popOut(activeID,call_from_page,transition){
+    var popout_container = document.getElementById("popout-container");
+    var grid_container = document.getElementById(activeID);
+    var active_element = document.getElementById(activeID+"-content");
+    var full_content = document.getElementsByClassName(activeID+"-content-full");
+    var newsy_container = document.getElementById("newsy-container");
+
+    if(transition){
+        popout_container.classList.add("transitions");
+        newsy_container.classList.add("transitions");
+    }
+
+    if(popout_container.hasChildNodes() && popout_state){
+        if(call_from_page && push_state){
+            history.pushState(activeID,"The Commons","http://127.0.0.1:5500/grid-test.html");
+            //console.log("push reset");
+        }
+        popout_state = 0;
+        push_state = 0;
+        var contentID = popout_container.firstChild.id;
+        var full_content = document.getElementsByClassName(contentID+"-full");
+        var activeID = contentID.split("-content").join("")
+        var active_element = document.getElementById(contentID);
+        var grid_container = document.getElementById(activeID);
+        for (var i = 0; i < full_content.length; i ++) {
+            full_content[i].style.opacity = 0;
+        }
+        resetPopoutSize(activeID);
         opacityToggle("1");
         setTimeout(function(){
             popout_container.style.display = "none";
             grid_container.appendChild(active_element);
             grid_container.style.visibility = "visible";
-        },1000);
+            push_state = 1;
+        },10);
     }
 
-    else{
-        if(popout_container.hasChildNodes()){
-            console.log("check");
+    //if the popout container is empty, and
+    else if(!popout_container.hasChildNodes() && !popout_state){
+        if(call_from_page){
+            history.pushState(activeID,activeID,"?pop="+activeID);
         }
-        else{
-            resetPopout(activeID);
-            popout_container.style.display = "block";
-            grid_container.style.visibility = "hidden";
-            opacityToggle("0");
-            popout_container.appendChild(active_element);
-            setTimeout(function(){
-                popout_container.style.width = document.getElementById("newsy-container").offsetWidth-20+"px";;
-                popout_container.style.height = document.getElementById("newsy-container").offsetHeight-20+"px";
-                popout_container.style.top = document.getElementById("newsy-container").offsetTop+10+"px";
-                popout_container.style.left = "10px";
-            },50);
-            window.scrollTo(0, 0);
-        }
+        popout_state = 1;
+        resetPopoutSize(activeID);
+        popout_container.style.display = "block";
+        grid_container.style.visibility = "hidden";
+        opacityToggle("0");
+        popout_container.appendChild(active_element);
+        setTimeout(function(){
+            popout_container.style.width = newsy_container.offsetWidth-20+"px";
+            popout_container.style.height = newsy_container.offsetHeight-20+"px";
+            popout_container.style.top = newsy_container.offsetTop+10+"px";
+            popout_container.style.left = "10px";
+            for (var i = 0; i < full_content.length; i ++) {
+                full_content[i].style.opacity = 1;
+            }
+        },10);
+        window.scrollTo(0, 0);
     }
 }
 
@@ -41,7 +125,7 @@ function opacityToggle(opacity_level){
 }
 
 /*sets the popout container to the size of the grid element it is replacing*/
-function resetPopout(activeID){
+function resetPopoutSize(activeID){
     var popout_container = document.getElementById("popout-container");
     var grid_container = document.getElementById(activeID);
 
