0abbeea51674128af49bfede81f31691213cce98
riqbic <dukereduker@gmail.com>
2023-02-27 22:27:48 -0500

    more index testing

--- wp-content/themes/the-commons/index.php
+++ wp-content/themes/the-commons/index.php
@@ -1,14 +1,3 @@
 <?php get_header(); ?>
-<!-- <div class="newsy-container grid-off-mobile">
-    <?php 
-    if ( have_posts() ) {
-        while ( have_posts() ) {
-            the_post(); ?>
-            <h1><?php the_title(); ?></h1>
-            <?php 
-            the_content();
-        } // end while
-    } // end if
-    ?>
-</div> -->
+
 <?php get_footer(); ?>
