8a55a0b4df4632a86f5be771f59f75d20e99aa6e
riqbic <dukereduker@gmail.com>
2023-02-28 23:13:19 -0500

    slight javascript fix

--- wp-content/themes/the-commons/js/commons-js-main.js
+++ wp-content/themes/the-commons/js/commons-js-main.js
@@ -109,7 +109,8 @@ function popOut(activeID,call_from_page,transition){
         popout_container.appendChild(active_element);
         setTimeout(function(){
             popout_container.style.width = newsy_container.offsetWidth-20+"px";
-            popout_container.style.height = newsy_container.offsetHeight-20+"px";
+            popout_container.style.minHeight = newsy_container.offsetHeight-20+"px";
+            popout_container.style.height = "auto";
             popout_container.style.top = newsy_container.offsetTop+10+"px";
             popout_container.style.left = newsy_container.offsetLeft+10+"px";
             for (var i = 0; i < full_content.length; i ++) {
