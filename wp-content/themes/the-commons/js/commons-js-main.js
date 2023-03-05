674a77cc077673a9613f05107c37d76c82d92579
riqbic <dukereduker@gmail.com>
2023-03-05 01:21:27 -0500

    changes for gregg

--- wp-content/themes/the-commons/js/commons-js-main.js
+++ wp-content/themes/the-commons/js/commons-js-main.js
@@ -2,42 +2,45 @@
 var popout_state = 0;
 var push_state = 1;
 
+//Pop state handler
 window.addEventListener('popstate', onPopState);
 function onPopState(ev) {
    activeID = ev.state;
    popOut(activeID,0,1);
 }
 
+//On page load, opens the pop out given by the URL paramater "pop", operates similarly to the popout funciton
 window.addEventListener('load',onPageLoad);
 function onPageLoad() {
+    //read URL param
     let url = window.location.search;
     let params = new URLSearchParams(url);
     var activeID = params.get("pop");
 
+    //declarations
     var popout_container = document.getElementById("popout-container");
     var grid_container = document.getElementById(activeID);
     var active_element = document.getElementById(activeID+"-content");
     var full_content = document.getElementsByClassName(activeID+"-content-full");
     var newsy_container = document.getElementById("newsy-container");
-
+    //pop out the content in the URL parameter
     if (params.getAll("pop").length !== 0){
-        //popOut(params.get("pop"),0,0);
         popout_state = 1;
         popout_container.style.display = "block";
         grid_container.style.visibility = "hidden";
         popout_container.appendChild(active_element);
+        active_element.style.display = "block";
         popout_container.style.width = newsy_container.offsetWidth-20+"px";
         popout_container.style.minHeight = newsy_container.offsetHeight-20+"px";
         popout_container.style.height = "auto";
         popout_container.style.top = newsy_container.offsetTop+10+"px";
         popout_container.style.left = newsy_container.offsetLeft+10+"px";
-        for (var i = 0; i < full_content.length; i ++) {
-            full_content[i].style.opacity = 1;
-        }
     }
+    //don't remember why this is here but it's important (?)
     resize();
 }
 
+//turn off transitions after a transition has finished
 window.addEventListener('transitionend',transitionToggle);
 function transitionToggle(){
     var newsy_container = document.getElementById("newsy-container");
@@ -46,36 +49,45 @@ function transitionToggle(){
     newsy_container.classList.remove("transitions");
 }
 
+//Resize the popout container (and a few other things) when the page is resized
 window.addEventListener('resize',resize);
 function resize(){
     var about_us_flex_item = document.getElementsByClassName("about-us-flex-item");
     var about_us_grid_item = document.getElementById("about-us");
     var popout_container = document.getElementById("popout-container");
+    popout_container.style.left = newsy_container.offsetLeft+10+"px";
     popout_container.style.width = document.getElementById("newsy-container").offsetWidth-20+"px";
+    popout_container.style.top = newsy_container.offsetTop+10+"px";
     for (var i = 0; i < about_us_flex_item.length; i ++) {
         about_us_flex_item[i].style.width = about_us_grid_item.offsetWidth+"px";
     }
 }
 
 
-//params (the ID of the grid item being popped out, did the function call come from the page? 0 or 1, should the transition play? 0 or 1)
+//popOut(the ID of the grid item being popped out, did the function call come from the page? 0 or 1, should the transition play? 0 or 1)
 function popOut(activeID,call_from_page,transition){
+    //declarations
     var popout_container = document.getElementById("popout-container");
     var grid_container = document.getElementById(activeID);
     var active_element = document.getElementById(activeID+"-content");
     var preview_element = document.getElementById(activeID+"-preview");
     var newsy_container = document.getElementById("newsy-container");
 
+    //turn on transitions if specified in params
     if(transition){
         popout_container.classList.add("transitions");
         newsy_container.classList.add("transitions");
     }
 
+    //if the popout container is populated, and we are not already doing a popout
+    //then "un-popout" the active content
     if(popout_container.hasChildNodes() && popout_state){
+        
+        //push the history stack, as long as the call came from the user, and not from a history push
         if(call_from_page && push_state){
             history.pushState(activeID,"The Commons","https://thecommons.boston");
-            //console.log("push reset");
         }
+
         popout_state = 0;
         push_state = 0;
         var contentID = popout_container.firstChild.id;
@@ -84,6 +96,7 @@ function popOut(activeID,call_from_page,transition){
         var grid_container = document.getElementById(activeID);
         resetPopoutSize(activeID);
         opacityToggle("1");
+        
         setTimeout(function(){
             popout_container.style.display = "none";
             grid_container.appendChild(active_element);
@@ -93,11 +106,15 @@ function popOut(activeID,call_from_page,transition){
         },10);
     }
 
-    //if the popout container is empty, and
+    //if the popout container is empty, and we are not currently do a pop out
+    //then add the content to the popout container, and pop it out.
     else if(!popout_container.hasChildNodes() && !popout_state){
+        
+        //push the history stack, as long as the call came from the user, and not from a history push
         if(call_from_page){
             history.pushState(activeID,activeID,"?pop="+activeID);
         }
+
         popout_state = 1;
         resetPopoutSize(activeID);
         popout_container.style.display = "block";
@@ -105,6 +122,8 @@ function popOut(activeID,call_from_page,transition){
         opacityToggle("0");
         popout_container.appendChild(active_element);
         active_element.style.display = "block";
+        
+        //the setTimeout just forces this code to run syncronously
         setTimeout(function(){
             popout_container.style.width = newsy_container.offsetWidth-20+"px";
             popout_container.style.minHeight = newsy_container.offsetHeight-20+"px";
@@ -112,17 +131,18 @@ function popOut(activeID,call_from_page,transition){
             popout_container.style.top = newsy_container.offsetTop+10+"px";
             popout_container.style.left = newsy_container.offsetLeft+10+"px";
         },10);
+
         window.scrollTo(0, 0);
     }
 }
 
-/*toggle the opacity of the grid*/
+//toggle the opacity of the grid
 function opacityToggle(opacity_level){
     var grid = document.getElementById("newsy-container");
     grid.style.opacity = opacity_level;
 }
 
-/*sets the popout container to the size of the grid element it is replacing*/
+//sets the popout container to the size of the grid element it is replacing
 function resetPopoutSize(activeID){
     var popout_container = document.getElementById("popout-container");
     var grid_container = document.getElementById(activeID);
