0804bd113bd6adbc19883c78fe1b697621f5e315
riqbic <dukereduker@gmail.com>
2023-03-07 13:30:56 -0500

    fix index

--- wp-content/themes/the-commons/index.php
+++ wp-content/themes/the-commons/index.php
@@ -1,14 +1,12 @@
 <?php get_header(); ?>
-<div class="newsy-container grid-off-mobile">
-    <?php 
-    if ( have_posts() ) {
-        while ( have_posts() ) {
-            the_post(); ?>
-            <h1><?php the_title(); ?></h1>
-            <?php 
-            the_content();
-        } // end while
-    } // end if
-    ?>
-</div>
+<?php 
+if ( have_posts() ) {
+    while ( have_posts() ) {
+        the_post(); ?>
+        <h1><?php the_title(); ?></h1>
+        <?php 
+        the_content();
+    } // end while
+} // end if
+?>
 <?php get_footer(); ?>
