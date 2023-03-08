6af45be12c3acc3182f7eeaf1b95631ce1017948
riqbic <dukereduker@gmail.com>
2023-03-07 22:42:57 -0500

    preview

--- wp-content/themes/the-commons/front-page.php
+++ wp-content/themes/the-commons/front-page.php
@@ -125,7 +125,7 @@
             <?php 
                     //Query 3 most recent posts that are published
                     $post_args = array(
-                        'posts_per_page'	=> -1,
+                        'posts_per_page'	=> 1,
                         'post_type'		=> 'post',
                         'post_status' => 'publish',
                         'cat' => 26,
