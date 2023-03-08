f489550d5b3f894995f1f71d8226d995fc74fb97
riqbic <dukereduker@gmail.com>
2023-03-07 22:35:07 -0500

    preivew test

--- wp-content/themes/the-commons/front-page.php
+++ wp-content/themes/the-commons/front-page.php
@@ -122,6 +122,36 @@
     <div class="grid-item" id="blog-alt">
         <div class="grid-preview" id="blog-alt-preview"  onclick="popOut('blog-alt',1,1)">
             <h2>Blog</h2>
+            <?php 
+                    //Query 3 most recent posts that are published
+                    $post_args = array(
+                        'posts_per_page'	=> -1,
+                        'post_type'		=> 'post',
+                        'post_status' => 'publish',
+                        'cat' => 26,
+                    );
+                    $posts_query = new WP_Query( $post_args );
+                    $blogct = 0;
+                    if( $posts_query->have_posts() ) {
+                        ++$blogct;
+                        //Declare an iterator for blog-item class
+                        while($posts_query->have_posts() ) {
+                            $posts_query->the_post(); 
+                            ?>
+                            <div class="blog-item blog-item-1;" data-id="<?php echo get_the_ID(); ?>">
+                            <h3><?php the_title(); ?></h3>
+                                <div class="newsy-small">
+                                    <?php the_excerpt(); ?>
+                                    <!--<a href="<?php echo get_permalink(); ?>" title="<?php echo get_the_title(); ?>">Read More</a>-->
+                                    READ MORE
+                                </div>
+                            </div>
+                            <?php 
+                        }
+                    } else { ?>
+                        <p>There are no posts to show right now.</p>
+                    <?php } ?>
+                </div><!-- close sidebar -->
         </div>
         <div class="grid-content" id="blog-alt-content">
             <div class="content-title">
