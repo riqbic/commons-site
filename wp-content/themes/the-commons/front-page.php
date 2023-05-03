30ee4eb1a7adfa6a8a50b4c24f38bef5a4bd7a88
riqbic <dukereduker@gmail.com>
2023-05-03 19:13:52 -0400

    popout title bar

--- wp-content/themes/the-commons/front-page.php
+++ wp-content/themes/the-commons/front-page.php
@@ -33,7 +33,8 @@
             </div>
             <div class="grid-content" id="blog-content">
                 <div class="popout-bar">
-                    <div class="popout-title">Articles | <?php echo the_title(); ?></div>
+                    <div class="popout-title">Articles</div>
+                    <div class="post-title"><?php echo the_title(); ?></div>
                     <div class="popout-close" onclick="popOut('blog',1,1)">Close</div>
                 </div>
                 <div class="blog-flex-container flex-row">
