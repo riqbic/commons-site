a891859d11f04285c23c010985a755a15cb47296
riqbic <dukereduker@gmail.com>
2023-05-11 20:21:07 -0400

    block test

---
+++ wp-content/themes/the-commons/blocks/blog-caption/blog-caption.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Testimonial Block Template.
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during backend preview render.
+ * @param   int $post_id The post ID the block is rendering content against.
+ *          This is either the post ID currently being displayed inside a query loop,
+ *          or the post ID of the post hosting this block.
+ * @param   array $context The context provided to the block by the post or it's parent block.
+ */
+
+// Support custom "anchor" values.
+$anchor = '';
+if ( ! empty( $block['anchor'] ) ) {
+    $anchor = 'id="' . esc_attr( $block['anchor'] ) . '" ';
+}
+
+// Create class attribute allowing for custom "className" and "align" values.
+$class_name = 'caption-block';
+if ( ! empty( $block['className'] ) ) {
+    $class_name .= ' ' . $block['className'];
+}
+if ( ! empty( $block['align'] ) ) {
+    $class_name .= ' align' . $block['align'];
+}
+
+// Load values and assign defaults.
+$text             = get_field( 'caption' ) ?: 'Your caption here...';
+$label           = get_field( 'caption-label' ) ?: 'Caption Label';
+$image            = get_field( 'image' ) ?: 295;
+
+// Build a valid style attribute for background and text colors.
+$styles = array( 'background-color: ' . $background_color, 'color: ' . $text_color );
+$style  = implode( '; ', $styles );
+
+?>
+<div <?php echo $anchor; ?>class="<?php echo esc_attr( $class_name ); ?>" style="<?php echo esc_attr( $style ); ?>">
+        <div class="testimonial-text"><?php echo esc_html( $text ); ?></div>
+        <div class="testimonial-author"><?php echo esc_html( $label ); ?></div>
+        <div class="testimonial-role"><?php echo esc_html( $image ); ?></div>
+</div>
