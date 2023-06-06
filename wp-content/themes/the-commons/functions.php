6b18f91fa835c30ed959e9966b1ad4871243ea91
riqbic <dukereduker@gmail.com>
2023-06-06 16:51:39 -0400

    idk

--- wp-content/themes/the-commons/functions.php
+++ wp-content/themes/the-commons/functions.php
@@ -40,6 +40,7 @@ function commons_theme_assets() {
  */
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
 
 add_action( 'wp_ajax_nopriv_load_commons_blog_post', 'load_commons_blog_post' );
 add_action( 'wp_ajax_load_commons_blog_post', 'load_commons_blog_post' );
