5e12476a74b232a9fd46d4d93afa11cc11cd1986
riqbic <dukereduker@gmail.com>
2023-06-22 12:19:45 -0400

    get latest post

--- wp-content/themes/the-commons/functions.php
+++ wp-content/themes/the-commons/functions.php
@@ -97,7 +97,8 @@ function get_latest_post_link(){
         'offset'          => 0,
         'orderby'         => 'post_date',
         'order'           => 'DESC',
-        'post_status'     => 'publish' );
+        'post_status'     => 'publish'
+        'category'        => '31'  );
     $sorted_posts = get_posts( $args );
     $permalink = get_permalink($sorted_posts[0]->ID);
     $post = $placeholder;
