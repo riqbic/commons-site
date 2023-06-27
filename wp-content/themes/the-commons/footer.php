ca5195ca89c5942717730254127fd818bd660fce
riqbic <dukereduker@gmail.com>
2023-06-27 16:18:19 -0400

    style

--- wp-content/themes/the-commons/footer.php
+++ wp-content/themes/the-commons/footer.php
@@ -2,9 +2,9 @@
 <div class="tcb-footer">
     <div class="socials-container">
         <p style="font-family: CenturyOldStyleRegular; font-size: 1.2rem; color: white;">links:</p>
-        <a href ="https://www.instagram.com/thecommons.boston/" class="menu-item">instagram</a>
-        <a href ="https://discord.com/invite/6wzh8Gbqxt" class="menu-item">discord</a>
-        <a href ="https://www.youtube.com/@thecommonsboston2770" class="menu-item">youtube</a>
+        <a href ="https://www.instagram.com/thecommons.boston/" class="menu-item" target="_blank">instagram</a>
+        <a href ="https://discord.com/invite/6wzh8Gbqxt" class="menu-item" target="_blank">discord</a>
+        <a href ="https://www.youtube.com/@thecommonsboston2770" class="menu-item" target="_blank">youtube</a>
     </div>
 </div>
 </body>
