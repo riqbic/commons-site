ed92298630568c5bff06374665d5dd55fbaf4be8
riqbic <dukereduker@gmail.com>
2023-10-23 19:57:00 -0400

    just guessing honestly

--- wp-content/themes/the-commons/js/home.js
+++ wp-content/themes/the-commons/js/home.js
@@ -136,10 +136,6 @@ jQuery.extend(jQuery.fn, {
                 popOut(popout_container.firstChild.id,1,1);
                 setTimeout(function(){
                     popOut('paid-videos',1,1);
-                    loadedPost = 0;
-                    //set the post content to use in the popout 
-                    var $this = $(this);
-                    setActivePopoutPost($this);
                 },50);
             }
             else{
@@ -149,6 +145,10 @@ jQuery.extend(jQuery.fn, {
                 var $this = $(this);
                 setActivePopoutPost($this);
             }
+            loadedPost = 0;
+            //set the post content to use in the popout 
+            var $this = $(this);
+            setActivePopoutPost($this);
         });
 
         //load the post in the popout contain when it's sidebar item is clicked
