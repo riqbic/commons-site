5ac4064d7af2ada00555624c4130acc69038ccb5
riqbic <dukereduker@gmail.com>
2023-12-18 15:10:44 -0500

    take 2

--- wp-content/themes/the-commons/blocks/blog-caption/blog-caption.php
+++ wp-content/themes/the-commons/blocks/blog-caption/blog-caption.php
@@ -25,12 +25,14 @@ if ( ! empty( $block['align'] ) ) {
 // Load values and assign defaults.
 $text             = get_field( 'caption' ) ?: '';
 $label           = get_field( 'caption-label' ) ?: '';
-$image_id            = get_field( 'figure' ) ?: "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="; //inserts a blank 26 byte gif if no image specified
+$image_id            = get_field( 'figure' ) ?: 295;
 
 ?>
 <div class="<?php echo esc_attr( $class_name )?>" id="anchor-<?php echo $id?>">
     <div class="caption-container" id="<?php echo $id?>">
-        <img class="caption-image" src="<?php echo esc_html( $image_id ); ?>" width="15%" height="auto">?>
+        <?php if ($image_id !== 295) : ?>
+            <img class="caption-image" src="<?php echo esc_html($image_id); ?>" width="15%" height="auto">
+        <?php endif; ?>
         <div class="caption-label"><?php echo esc_html( $label ); ?></div>
         <div class="caption-text"><?php echo esc_html( $text ); ?></div>
     </div>
